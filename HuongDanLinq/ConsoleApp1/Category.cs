683ef9d5d9c76aebed1e6840ca7fc9fb889ba1bc
tri1803 <triitno1@gmail.com>
2022-08-17 21:30:16 +0700

    Hướng dẫn linq

---
+++ HuongDanLinq/ConsoleApp1/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Category
+    {
+        public string Name { set; get; }
+        public int ID { set; get; }
+    }
+}
---
+++ HuongDanLinq/ConsoleApp1/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Student : Person
+    {
+        public override void DemSoLuongChanTay(int chan, int tay)
+        {
+            Console.WriteLine("4");
+        }
+
+        public override void Speak()
+        {
+            Console.WriteLine("Đây là con người");
+        }
+    }
+}
